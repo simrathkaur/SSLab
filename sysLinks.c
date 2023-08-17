be543dfb926a77996341f5aa95f4ba56ab74390c
simrathkaur <simrathkaur9@gmail.com>
2023-08-17 19:08:08 +0530

    made files of type softlink, hardlink, Fifo using system calls, and fifo file using mkfifo

---
+++ sysLinks.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+int main(){
+       /* int h=link("original.txt", "syshardlink");
+        if(h<0) {
+                perror("Failed"); return 1;
+        }
+        int s=symlink("original.txt","syssoftlink");
+        if(s<0){ perror("Failed"); return 1;}
+*/
+        int f=mknod("sysFifoFile",S_IFIFO,0);
+        if(f<0) perror("Failed");
+        return 0;
+        }
+
