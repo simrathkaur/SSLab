c90ea81686e577064c27e39a4c08f24e55e59509
simrathkaur <simrathkaur9@gmail.com>
2023-08-17 21:02:04 +0530

    hands on 1-> 5 done, five files created, infinite loop, proc stat checked

---
+++ handon1.5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+int main(){
+	
+		int f1=creat("temp1",0744);
+		int f2=creat("temp2",0744);
+		int f3=creat("temp3",0744);
+		int f4=creat("temp4",0744);
+		int f5=creat("temp5",0744);
+	for(;;);
+}
+
