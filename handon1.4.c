7e025a5a95e4b6dae5297a7608786e48d5e41cac
simrathkaur <simrathkaur9@gmail.com>
2023-08-17 20:10:32 +0530

    O_RDWR and O_EXCL flag used

---
+++ handon1.4.c
@@ -0,0 +1,9 @@
+#include<fcntl.h>
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+        int fd1= open("temp", O_RDWR,O_EXCL);
+        printf("the file descripter value of temp file:%d", fd1);
+        return 0;
+}
+
